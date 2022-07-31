2a9867b16727cf6e4e510ffb282f8bc42c85d7ee
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-07-31 00:14:54 -0300

    feat: only allow existing user and admin list all users

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,15 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User does not exists!");
+    }
+    if (!user.admin) {
+      throw new Error("No permission to list users.");
+    }
+    return this.usersRepository.list();
   }
 }
 
