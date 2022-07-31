1464ddc6fe98f190ea25bba6ca15dec130f2138c
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-07-31 00:02:30 -0300

    feat: create new users

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,7 +19,17 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const newUser = new User();
+
+    Object.assign(newUser, {
+      name,
+      email,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    this.users.push(newUser);
+    return newUser;
   }
 
   findById(id: string): User | undefined {
