ae8a96a7561ca84a57150347694900f1414602b1
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-07-31 00:05:35 -0300

    feat: turn an user as admin

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -42,7 +42,9 @@ class UsersRepository implements IUsersRepository {
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    const user = this.users.find((user) => user.id === receivedUser.id);
+    user.admin = true;
+    return user;
   }
 
   list(): User[] {
